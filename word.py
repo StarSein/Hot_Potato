787191abea90033d558b7ff4d8bb802de5028f7a
Yuseong Hwang <75326335+HwangYooSung@users.noreply.github.com>
2021-06-15 00:35:19 +0900

    21-1 final-term problem solving
    The requirement was sorting of a given list consisting of words which are combined with digit or not. This source code is how I solve the problem that I couldn't deal with during final-term.
    Through this problem, I have learned the importance of knowing how to implement 'sorting'. I have used 'sort()' familiarly and neglected understanding the algorithm of the function. There are 3 basic style of sorting - selection sort, insertion sort, and bubble sort. The third one is what I utilized editing my source code.

---
+++ word.py
@@ -0,0 +1,61 @@
+with open('word.inp', 'r') as inFile:
+    list_word = list(inFile.readline().split())
+
+# 정렬 기준 : 길이(짧은것부터) > 문자(대문자 우선, 백과사전순) > 숫자(작은수부터)
+
+
+def sorting():
+    for count in range(1, len(list_word) + 1):
+        for i in range(len(list_word) - count):
+            list_word[i], list_word[i+1] = compare(list_word[i], list_word[i+1])
+            print(*list_word)
+
+
+def compare(a, b):
+    if len(a) > len(b):
+        a, b = b, a
+    elif len(a) < len(b):
+        pass
+    else:
+        for idx in range(len(a)):
+            if a[idx] == b[idx]:
+                continue
+
+            if a[idx].isdigit() and not b[idx].isdigit():
+                a, b = b, a
+                break
+            elif not a[idx].isdigit() and b[idx].isdigit():
+                break
+            else:
+                if a[idx].isdigit() and b[idx].isdigit():
+                    if int(a[idx]) > int(b[idx]):
+                        a, b = b, a
+                        break
+                elif a[idx].isupper() and b[idx].isupper():
+                    if a[idx] > b[idx]:
+                        a, b = b, a
+                        break
+                    else:
+                        break
+                elif a[idx].islower() and b[idx].isupper():
+                    a, b = b, a
+                    break
+                elif a[idx].isupper() and b[idx].islower():
+                    break
+                else:
+                    if a[idx] > b[idx]:
+                        a, b = b, a
+                        break
+                    else:
+                        break
+    return a, b
+
+
+sorting()
+
+result = ""
+for item in list_word:
+    result += "{} ".format(item)
+
+with open('word.out', 'w') as outFile:
+    outFile.write(result.rstrip())
